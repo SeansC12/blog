252fb1551c518e70acea4d85ec692efe161f8884
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-03-29 07:33:08 +0800

    added markup and preview buttons

--- pages/Create.js
+++ pages/Create.js
@@ -8,9 +8,15 @@ function Create() {
   const [blogData, setBlogData] = useState();
 
   return (
-    <div className="bg-white">
-      <Editor data={blogData} onChange={setBlogData} holder="editorjs-container" />
-      <div>Hello</div>
+    <div>
+      <div className="bg-gradient-to-b from-[#0e141b] to-white h-6" />
+      <div className="flex items-center justify-center gap-5 bg-white">
+        <div className="text-black">Markup</div>
+        <div className="text-black">Preview</div>
+      </div>
+      <div className="bg-white">
+        <Editor data={blogData} onChange={setBlogData} holder="editorjs-container" />
+      </div>
     </div>
   )
 }
