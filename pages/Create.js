330313acc3dcbda9de5529c3ac6eecdf0fb13cd7
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-04-01 14:48:00 +0800

    created modal for users to change description

--- pages/Create.js
+++ pages/Create.js
@@ -16,7 +16,9 @@ function Create() {
   const [blogTitle, setBlogTitle] = useState();
   const [isEditingMode, setIsEditingMode] = useState(true);
   const [randomState, setRandomState] = useState(false);
+  const [isPublishedMode, setIsPublishedMode] = useState(false);
   const titleRef = useRef();
+  const descriptionRef = useRef();
   const router = useRouter();
 
   async function publish(title, blog) {
@@ -48,7 +50,7 @@ function Create() {
         blog: blog,
         person_id: user.id,
         url_name: finalURLName,
-        description: "Test 1"
+        description: descriptionRef.current.value
       })
     })
 
@@ -73,8 +75,39 @@ function Create() {
 
   return (
     <div>
+      {isPublishedMode ?
+        <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div class="sm:flex sm:items-start">
+                    {/* <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                      </svg>
+                    </div> */}
+                    <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left grow">
+                      <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Adding the finishing touches...</h3>
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">Give your blog article a description</p>
+                      </div>
+                      <input ref={descriptionRef} className="border-2 rounded w-full my-4 text-black" />
+                    </div>
+                  </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button onClick={() => publish(titleRef.current.value, blogData)} type="button" class="inline-flex w-full justify-center rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-400 sm:ml-3 sm:w-auto">Publish</button>
+                  <button onClick={() => setIsPublishedMode(false)} type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> : null}
+
       <div className="flex justify-end items-center">
-        <div onClick={() => publish(titleRef.current.value, blogData)} className="rounded-md bg-green-400 py-[0.35rem] px-4 text-black font-lato w-max mr-5 hover:scale-105 transition-all ease-in cursor-pointer select-none">
+        <div onClick={() => setIsPublishedMode(true)} className="rounded-md bg-green-400 py-[0.35rem] px-4 text-black font-lato w-max mr-5 hover:scale-105 transition-all ease-in cursor-pointer select-none">
           Publish
         </div>
       </div>
