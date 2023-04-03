99bd4574046b2324785f1fb2c7fa5582330c33b3
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-04-03 16:46:05 +0800

    added image

--- components/EditorTools.js
+++ components/EditorTools.js
@@ -2,10 +2,12 @@ import Code from "@editorjs/code";
 import Header from "@editorjs/header";
 import Paragraph from "@editorjs/paragraph";
 import Quote from "@editorjs/quote";
+import SimpleImage from "@editorjs/simple-image";
 
 export const EDITOR_TOOLS = {
   code: Code,
   header: Header,
   paragraph: Paragraph,
   quote: Quote,
+  image: SimpleImage,
 };
