45a080263936d242b832287aa964b71831a362cf
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-04-04 07:33:21 +0800

    replaced next/image with standard html img tag

--- pages/[person]/index.js
+++ pages/[person]/index.js
@@ -7,7 +7,7 @@ export default function Home({ data, person }) {
   return (
     <div className="px-96">
       <div className="w-full flex items-center justify-center flex-col gap-5">
-        <Image src={`https://ui-avatars.com/api/?name=${person && person}`} width={150} height={150} className="aspect-square rounded-full" />
+        <img src={`https://ui-avatars.com/api/?name=${person && person}`} width={150} height={150} className="aspect-square rounded-full" />
         <h1 className="font-bold text-3xl">{person}</h1>
       </div>
       {data && data.map((blog, key) => {
