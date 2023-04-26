e28d03e249bff3c38e6bdba8d1d629e8b8b92a6b
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-04-26 11:48:14 +0800

    pushing final changes

--- pages/Create.js
+++ pages/Create.js
@@ -58,6 +58,28 @@ function Create() {
     setTempBlogData(blogData)
   }, [isEditingMode])
 
+  useEffect(() => {
+    const warningText =
+      'You have unsaved changes - are you sure you wish to leave this page?';
+    const handleWindowClose = (e) => {
+      // if (!unsavedChanges) return;
+      e.preventDefault();
+      return (e.returnValue = warningText);
+    };
+    const handleBrowseAway = () => {
+      // if (!unsavedChanges) return;
+      if (window.confirm(warningText)) return;
+      router.events.emit('routeChangeError');
+      throw 'routeChange aborted.';
+    };
+    window.addEventListener('beforeunload', handleWindowClose);
+    router.events.on('routeChangeStart', handleBrowseAway);
+    return () => {
+      window.removeEventListener('beforeunload', handleWindowClose);
+      router.events.off('routeChangeStart', handleBrowseAway);
+    };
+  });
+
   return (
     <div className="h-full">
       {isPublishedMode ?
--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -23,6 +23,29 @@ function id({ data }) {
   const titleRef = useRef();
   const descriptionRef = useRef();
 
+  // prompt the user if they try and leave with unsaved changes
+  useEffect(() => {
+    const warningText =
+      'You have unsaved changes - are you sure you wish to leave this page?';
+    const handleWindowClose = (e) => {
+      // if (!unsavedChanges) return;
+      e.preventDefault();
+      return (e.returnValue = warningText);
+    };
+    const handleBrowseAway = () => {
+      // if (!unsavedChanges) return;
+      if (window.confirm(warningText)) return;
+      router.events.emit('routeChangeError');
+      throw 'routeChange aborted.';
+    };
+    window.addEventListener('beforeunload', handleWindowClose);
+    router.events.on('routeChangeStart', handleBrowseAway);
+    return () => {
+      window.removeEventListener('beforeunload', handleWindowClose);
+      router.events.off('routeChangeStart', handleBrowseAway);
+    };
+  }, []);
+
   async function update(title, description, blog) {
     // Get the user
     const { data: { user } } = await supabaseClient.auth.getUser();
